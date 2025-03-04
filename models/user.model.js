6be5339a268d5ed41a94dc9167dd17231a025403
Milos Srejic <srejic.milos94@gmail.com>
2025-03-04 20:59:00 +0100

    Create user schema

---
+++ models/user.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      unique: true,
+      minLength: 2,
+      maxLength: 40,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minLength: 6,
+    },
+    email: {
+      type: String,
+      required: [true, "Email address is required"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: [/\S+\@\.\S+/, "Please enter a valid email address"],
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
