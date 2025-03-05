2ce629708784b97c574b0c4f9856ccf41b1617ea
Milos Srejic <srejic.milos94@gmail.com>
2025-03-05 19:27:38 +0100

    Add middleware for JSON parsing, URL-encoded data, and cookie handling

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ import authRouter from "./routes/auth.routes.js";
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/todos", todosRouter);
