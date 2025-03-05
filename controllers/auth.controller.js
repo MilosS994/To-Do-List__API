f2a34d33d62736ba5c0c37ac06409e92afb0187c
Milos Srejic <srejic.milos94@gmail.com>
2025-03-05 20:50:04 +0100

    Implement user login controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -55,6 +55,39 @@ export const register = async (req, res, next) => {
   }
 };
 
-export const login = async (req, res, next) => {};
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    // If user does not exist
+    if (!user) {
+      const error = new Error("User not found");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      const error = new Error("Invalid password");
+      error.statusCode = 401;
+      throw error;
+    }
+
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
+      expiresIn: JWT_EXPIRES_IN,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "User signed in successfully",
+      data: { token, user },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const logout = async (req, res, next) => {};
